8cfc158f1ce89369bbe9fcf971765ab1b7c1b81e
Gervis Maldonado <gervislm@icloud.com>
2022-12-06 13:56:50 -0300

    double equality

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import '../styles/Button.css'
 function Button(props) {
 
   const isOperator = value => {
-    return isNaN(value) && (value != '.');
+    return isNaN(value) && (value !== '.');
   };
 
   return (
